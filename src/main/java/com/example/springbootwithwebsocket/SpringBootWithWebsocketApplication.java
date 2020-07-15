6548c104abf7f5c909ee136bef2485ea9c10ce60
Catalina <catalina@Catalinas-Mac.local>
2020-07-15 15:43:47 +0700

    Spring boot chat with websocket

---
+++ src/main/java/com/example/springbootwithwebsocket/SpringBootWithWebsocketApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootwithwebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithWebsocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWithWebsocketApplication.class, args);
+    }
+
+}
